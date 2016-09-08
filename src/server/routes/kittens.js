11d5a544765a260f3e9487f9d95abe14ad2cf9e8
Ryan "archaen" Pando <vretha@gmail.com>
2016-09-07 21:08:51 -0600

    Data being retrieved from database.

--- src/server/routes/kittens.js
+++ src/server/routes/kittens.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
-const indexController = require('../controllers/kittens');
+const kittenController = require('../controllers/kittens');
+const knex = require('../db/knexfile');
 
 router.get('/', function (req, res, next) {
-  res.send('here');
+  const renderObject = {};
+  renderObject.title = 'Kittens';
+  knex('kittens')
+    .then(animals => {
+      renderObject.data = animals;
+      res.render('kittens', renderObject);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
